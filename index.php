88ba1e12ee7590a9ea20a5143f34fab2cfac98a0
Giulia <giulia.guerrazzi@live.it>
2020-12-17 16:00:47 +0100

    setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Disc PHP</title>
+</head>
+<body>
+
+    <div id='app'>
+    
+    </div>
+   <?php include __DIR__ . '/scripts/database.php'; ?>
+
+
+
+   <script src="./dist/js/main.js"></script>
+</body>
+</html>
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import axios from 'axios';
+
+// console.log(axios);
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+       
+    }
+});
